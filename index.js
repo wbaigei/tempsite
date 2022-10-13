d64b51fcac8381a3cdb91ca59a03b4c63ac00768
ReTrn <84667504+wbaigei@users.noreply.github.com>
2022-10-13 17:12:20 +0800

    1013

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 document.getElementById("submit").click = () => {
-    var body = document.getElementById("s").value
+    var body = document.getElementById("s").value;
     var link = `https://xfeedback.herokuapp.com/send?topic=NONE&user=[USERNAME]&message=${body}&api=MfDTklWx9FEJ1ghVd7n57egFZ9FJT5thbqsD9Dp8Y7kF1yjuZe8-glVeh2zRN15D`
+    console.log(link);
     fetch(link).then(()=>{
         console.log("done")
     });
